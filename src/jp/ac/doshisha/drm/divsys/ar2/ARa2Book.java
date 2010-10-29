76b9561767752994faab0b2fe2cba029dd0a5107
Yuya Tanaka <yuya.tanaka.i@gmail.com>
2010-10-29 16:36:27 +0900

    Many updates including multi-node
    Signed-off-by: Yuya Tanaka <yuya.tanaka.i@gmail.com>

--- src/jp/ac/doshisha/drm/divsys/ar2/ARa2Book.java
+++ src/jp/ac/doshisha/drm/divsys/ar2/ARa2Book.java
@@ -24,16 +24,13 @@ import javax.media.opengl.GLEventListener;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import jp.nyatla.kGLModel.KGLExtensionCheck;
-import jp.nyatla.kGLModel.KGLModelData;
-import jp.nyatla.kGLModel.contentprovider.ContentProvider;
 import jp.nyatla.kGLModel.contentprovider.HttpContentProvider;
 import jp.nyatla.nyarmqoviewer.utils.SimpleXPath;
 import jp.nyatla.nyartoolkit.NyARException;
 import jp.nyatla.nyartoolkit.core.NyARCode;
 import jp.nyatla.nyartoolkit.core.param.NyARParam;
 import jp.nyatla.nyartoolkit.core.transmat.NyARTransMatResult;
-import jp.nyatla.nyartoolkit.detector.NyARSingleDetectMarker;
+import jp.nyatla.nyartoolkit.detector.NyARDetectMarker;
 import jp.nyatla.nyartoolkit.jmf.utils.JmfCaptureDevice;
 import jp.nyatla.nyartoolkit.jmf.utils.JmfCaptureDeviceList;
 import jp.nyatla.nyartoolkit.jmf.utils.JmfCaptureListener;
@@ -41,6 +38,7 @@ import jp.nyatla.nyartoolkit.jmf.utils.JmfNyARRaster_RGB;
 import jp.nyatla.nyartoolkit.jogl.utils.NyARGLUtil;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import com.sun.opengl.util.Animator;
 class Logger
@@ -55,15 +53,13 @@ class NyARMqoViewerParam
 {
     public URL    base_url;
     public String version;
-    public String main_identifier;	//ContentProviderに渡すメイン識別子(moqファイルのアドレス)
-    public String arcode_identifier;	//
     public String cparam_identifier;
-    public double marker_size;
-    public float scale;
+    public ARa2Node[] node_array;
     public int screen_x;
     public int screen_y;
     public float frame_rate;
     public String comment;
+	public double confidence;
     public NyARMqoViewerParam(URL i_url) throws NyARException
     {
 	Logger.logln("メタデータに接続中\r\n->"+i_url);
@@ -95,18 +91,27 @@ class NyARMqoViewerParam
 	    version=root.selectString("version");
 	    //Config読み出し
 	    tmp=root.select("config");
-	    this.arcode_identifier=tmp.selectString("ar_code/url");
-	    this.marker_size=tmp.selectDouble("ar_code/size");
 	    this.cparam_identifier=tmp.selectString("ar_param/url");
 	    this.screen_x=tmp.selectInt("ar_param/screen/x");
 	    this.screen_y=tmp.selectInt("ar_param/screen/y");
 	    this.frame_rate=(float)tmp.selectDouble("frame_rate");
+	    this.confidence=tmp.selectDouble("min_confidence");
+	    //Comment読み出し
+	    comment = root.selectString("comment");
 
-	    //content読み出し
-	    tmp=root.select("content");
-	    this.scale=(float)tmp.selectDouble("scale");
-	    this.main_identifier=tmp.selectString("mqo_file");
-	    this.comment=tmp.selectString("comment");
+	    //Nodes読み出し
+	    NodeList tmp_nl = root.selectNodeSet("nodes/node");
+	    node_array = new ARa2Node[tmp_nl.getLength()];
+	    for (int i = 0; i < node_array.length; i++) {
+	    	node_array[i] = new ARa2Node();
+		    SimpleXPath tmp_node = new SimpleXPath(tmp_nl.item(i));
+		    node_array[i].main_identifier = tmp_node.selectString("mqo_file");
+		    node_array[i].arcode_identifier = tmp_node.selectString("ar_code/url");
+		    node_array[i].marker_size = tmp_node.selectDouble("ar_code/size");
+		    node_array[i].scale = (float) tmp_node.selectDouble("scale");
+		    node_array[i].comment = tmp.selectString("comment");
+		    node_array[i].loadExternalData(base_url);
+	    }
 
 	}catch (Exception e){
 	    throw new NyARException(e);
@@ -116,25 +121,29 @@ class NyARMqoViewerParam
     }
     private void validationCheck() throws NyARException
     {
-	if(version.compareTo("NyARMqoViewer/0.1")!=0){
-	    throw new NyARException("バージョン不一致 NyARMqoViewer/0.1である必要があります。");
-	}
-	if(this.marker_size<5.0f || this.marker_size>1000.0){
-	    throw new NyARException("マーカーサイズが不正 5.0<size<1000.0である必要があります。");
-	}
-	if(	(this.screen_x==160 && this.screen_y==120) ||
-		(this.screen_x==320 && this.screen_y==240) ||
-		(this.screen_x==640 && this.screen_y==480)){
-	}else{
-	    throw new NyARException("スクリーン幅が不正 160x120 320x240 640x480の何れかを指定してください。");
-	}
-
-	if(this.frame_rate<10 || this.frame_rate>60){
-	    throw new NyARException("フレームレートが不正 15.0 30.0ぐらいを指定して下さい。");
-	}
-	if(this.scale<0.01 || this.scale>100.0){
-	    throw new NyARException("スケールが不正 0.01-100.0ぐらいを指定して下さい。");
-	}	
+    	if(!version.equals("ARa2Book/0.1")){
+    		throw new NyARException("バージョン不一致 ARa2Book/0.1である必要があります。");
+    	}
+    	for (int i = 0; i < node_array.length; i++) {
+    		if(node_array[i].marker_size < 5.0f || node_array[i].marker_size > 1000.0){
+    			throw new NyARException("ノード"+ i + ": マーカーサイズが不正 5.0<size<1000.0である必要があります。");
+    		}
+    		if(node_array[i].scale < 0.01 || this.node_array[i].scale > 100.0){
+    			throw new NyARException("ノード"+ i + ": スケールが不正 0.01-100.0ぐらいを指定して下さい。");
+    		}
+    	}
+    	if(	(this.screen_x == 160 && this.screen_y == 120) ||
+    			(this.screen_x == 320 && this.screen_y == 240) ||
+    			(this.screen_x == 640 && this.screen_y == 480)){
+    	}else{
+    		throw new NyARException("スクリーン幅が不正 160x120 320x240 640x480の何れかを指定してください。");
+    	}
+    	if(this.frame_rate < 10 || this.frame_rate > 60){
+    		throw new NyARException("フレームレートが不正 15.0 30.0ぐらいを指定して下さい。");
+    	}
+    	if (this.confidence < 0 || this.confidence > 1) {
+    		throw new NyARException("一致度が不正 0.0-1.0を指定して下さい。");
+    	}
     }
 }
 
@@ -142,17 +151,15 @@ class NyARMqoViewerParam
 
 public class ARa2Book implements GLEventListener,JmfCaptureListener
 {
-    private int threshold;
+	private int threshold;
     private NyARMqoViewerParam app_param;
-    private KGLModelData model_data; // kei add
-    private ContentProvider content_provider;
     private Animator animator;
     private JmfNyARRaster_RGB cap_image;
     private JmfCaptureDevice capture;
     private GL gl;
     //NyARToolkit関係
     private NyARGLUtil glnya;
-    private NyARSingleDetectMarker nya;
+    private NyARDetectMarker nya;
     private NyARParam ar_param;
 
     /**
@@ -194,19 +201,11 @@ public class ARa2Book implements GLEventListener,JmfCaptureListener
 	this.ar_param=new NyARParam();
 	this.ar_param.loadARParam(createHttpStream(this.app_param.base_url,this.app_param.cparam_identifier));
 	this.ar_param.changeScreenSize(SCR_X,SCR_Y);
-	//検出マーカーの設定
-	NyARCode ar_code  =new NyARCode(16,16);
-	ar_code.loadARPatt(createHttpStream(this.app_param.base_url,this.app_param.arcode_identifier));
 	//マーカーDetecterの作成
 	this.capture.setCaptureFormat(SCR_X, SCR_Y, app_param.frame_rate);
 	this.cap_image=new JmfNyARRaster_RGB(this.ar_param, this.capture.getCaptureFormat());
-	this.nya=new NyARSingleDetectMarker(this.ar_param,ar_code,this.app_param.marker_size, cap_image.getBufferType());
-	//コンテンツプロバイダを作成
-	try{
-	    this.content_provider=new HttpContentProvider(this.app_param.main_identifier);
-	}catch(Exception e){
-	    throw new NyARException(e);
-	}
+	this.nya=createNyARDetectMarker();
+	this.nya.setContinueMode(true);
 	Logger.logln("ウインドウを作成しています.");
 	//ウインドウの準備
 	Frame frame = new Frame("ARa2Book");
@@ -230,9 +229,10 @@ public class ARa2Book implements GLEventListener,JmfCaptureListener
 	gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
 	Logger.logln("モデルデータに接続中です.");
-	try {// kei add
-	    model_data = KGLModelData.createGLModel(gl,null,this.content_provider,0.015f,
-		    KGLExtensionCheck.IsExtensionSupported(gl,"GL_ARB_vertex_buffer_object")) ;
+	try {
+		for (int i = 0; i < app_param.node_array.length; i++) {
+			app_param.node_array[i].createModelData(gl);
+		}
 	}
 	catch(Exception e) {
 	    e.printStackTrace() ;
@@ -275,48 +275,52 @@ public class ARa2Book implements GLEventListener,JmfCaptureListener
     public void display(GLAutoDrawable drawable)
     {
 
-	try{
-	    if(!cap_image.hasBuffer()){
-		return;
-	    }    
-	    gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT); // Clear the buffers for new frame.          
-	    //画像チェックしてマーカー探して、背景を書く
-	    boolean is_marker_exist;
-	    synchronized(cap_image){
-		is_marker_exist=nya.detectMarkerLite(cap_image,this.threshold);
-		//背景
-		glnya.drawBackGround(cap_image, 1.0);
-	    }
-	    //あったら立方体を書く
-	    if(is_marker_exist){
-		// Projection transformation.
-		gl.glMatrixMode(GL.GL_PROJECTION);
-		double[] glmat = new double[16];
-		glnya.toCameraFrustumRH(ar_param, glmat);
-		gl.glLoadMatrixd(glmat, 0);
-		gl.glMatrixMode(GL.GL_MODELVIEW);
-		// Viewing transformation.
-		gl.glLoadIdentity();
-		NyARTransMatResult nya_transmat_result = new NyARTransMatResult();
-		nya.getTransmationMatrix(nya_transmat_result);
-		glnya.toCameraViewRH(nya_transmat_result, glmat);
-		gl.glLoadMatrixd(glmat,0);
+    	try{
+    		if(!cap_image.hasBuffer()){
+    			return;
+    		}    
+    		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT); // Clear the buffers for new frame.          
+    		//画像チェックしてマーカー探して、背景を書く
+    		int num_marker_exist;
+    		synchronized(cap_image){
+    			num_marker_exist=nya.detectMarkerLite(cap_image,this.threshold);
+    			//背景
+    			glnya.drawBackGround(cap_image, 1.0);
+    		}
+    		//あったら立方体を書く
+    		if(num_marker_exist > 0){
+    			double[] glmat = new double[16];
+    			gl.glMatrixMode(GL.GL_PROJECTION);
+    			glnya.toCameraFrustumRH(ar_param, glmat);
+    			gl.glLoadMatrixd(glmat, 0);
+    			for (int i = 0; i < num_marker_exist; i++) {
+    				if (nya.getConfidence(i) < app_param.confidence)
+    					continue;
+    				ARa2Node node = app_param.node_array[nya.getARCodeIndex(i)];
+    				// Projection transformation.
+    				gl.glMatrixMode(GL.GL_MODELVIEW);
+    				// Viewing transformation.
+    				NyARTransMatResult nya_transmat_result = new NyARTransMatResult();
+    				nya.getTransmationMatrix(i, nya_transmat_result);
+    				glnya.toCameraViewRH(nya_transmat_result, glmat);
+    				gl.glLoadMatrixd(glmat,0);
 
-		// -------v------ kei add
-		gl.glTexEnvf(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE, GL.GL_MODULATE);
-		gl.glTranslatef(0.0f,0.0f,0.0f) ;//位置調整
-		gl.glRotatef(90.0f,1.0f, 0.0f, 0.0f); //OpenGL座標系→ARToolkit座標系
-		gl.glEnable(GL.GL_CULL_FACE);
-		gl.glCullFace(GL.GL_FRONT);
-		model_data.enables(app_param.scale) ;
-		model_data.draw() ;
-		model_data.disables() ;
-		// -----^^^------ kei add
-	    }
-	}catch(Exception e){
-	    e.printStackTrace();
-	}
-    }
+    				// -------v------ kei add
+    				gl.glTexEnvf(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE, GL.GL_MODULATE);
+    				gl.glTranslatef(0.0f,0.0f,0.0f) ;//位置調整
+    				gl.glRotatef(90.0f,1.0f, 0.0f, 0.0f); //OpenGL座標系→ARToolkit座標系
+    				gl.glEnable(GL.GL_CULL_FACE);
+    				gl.glCullFace(GL.GL_FRONT);
+    				node.model_data.enables(node.scale) ;
+    				node.model_data.draw() ;
+    				node.model_data.disables() ;
+    				// -----^^^------ kei add
+    			}
+    		}
+    		}catch(Exception e){
+    			e.printStackTrace();
+    		}
+    	}
     public void onUpdateBuffer(Buffer i_buffer)
     {
 	try{
@@ -333,6 +337,16 @@ public class ARa2Book implements GLEventListener,JmfCaptureListener
 	    boolean deviceChanged)
     {
 	
+    }
+    
+    protected NyARDetectMarker createNyARDetectMarker() throws NyARException {
+    	NyARCode[] ar_code_array = new NyARCode[app_param.node_array.length];
+    	double[] marker_size_array = new double[app_param.node_array.length];
+    	for(int i = 0; i < app_param.node_array.length; i++) {
+    		ar_code_array[i] = app_param.node_array[i].ar_code;
+    		marker_size_array[i] = app_param.node_array[i].marker_size;
+    	}
+    	return new NyARDetectMarker(ar_param, ar_code_array, marker_size_array, app_param.node_array.length, cap_image.getBufferType());
     }
     
     /**
@@ -369,7 +383,7 @@ public class ARa2Book implements GLEventListener,JmfCaptureListener
 	try{
 	    // URL接続
 	    NyARMqoViewerParam param=new NyARMqoViewerParam(new URL(target));
-	    System.out.println("==モデルデータの情報==\r\n"+param.comment);
+	    System.out.println("==メタデータの情報==\r\n"+param.comment);
 	    new ARa2Book(param,threshold);
 	} catch (Exception ex){
 	    System.err.println("エラーになっちゃった。");
---
+++ src/jp/ac/doshisha/drm/divsys/ar2/ARa2Node.java
@@ -0,0 +1,53 @@
+package jp.ac.doshisha.drm.divsys.ar2;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.media.opengl.GL;
+
+import jp.nyatla.kGLModel.KGLException;
+import jp.nyatla.kGLModel.KGLExtensionCheck;
+import jp.nyatla.kGLModel.KGLModelData;
+import jp.nyatla.kGLModel.contentprovider.ContentProvider;
+import jp.nyatla.kGLModel.contentprovider.HttpContentProvider;
+import jp.nyatla.nyartoolkit.NyARException;
+import jp.nyatla.nyartoolkit.core.NyARCode;
+
+public class ARa2Node {
+
+	public KGLModelData model_data;
+	public NyARCode ar_code;
+	public double marker_size;
+	public float scale;
+	public String main_identifier;
+	public ContentProvider main_content_provider;
+	public String arcode_identifier;
+	public ContentProvider arcode_content_provider;
+	public String comment;
+	private GL gl;
+
+	public ARa2Node() throws NyARException {
+		ar_code = new NyARCode(16, 16);
+	}
+
+	public void loadExternalData(URL i_base_url) throws KGLException, NyARException, MalformedURLException {
+		URL main_url;
+		URL arcode_url;
+		if (i_base_url != null) {
+			main_url = new URL(i_base_url, main_identifier);
+			arcode_url = new URL(i_base_url, arcode_identifier);
+		}
+		else {
+			main_url = new URL(main_identifier);
+			arcode_url = new URL(arcode_identifier);
+		}
+		main_content_provider = new HttpContentProvider(main_url.toString());
+		ar_code.loadARPatt(HttpContentProvider.createInputStream(arcode_url));
+	}
+
+	public void createModelData(GL i_gl) throws KGLException {
+		gl = i_gl;
+		model_data = KGLModelData.createGLModel(gl, null, main_content_provider,
+	    		0.015f, KGLExtensionCheck.IsExtensionSupported(gl,"GL_ARB_vertex_buffer_object"));
+	}
+}
